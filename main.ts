b76644b65bdc3cf527875bfc5b5b2045a98c081c
Aurelio Gallardo Rodríguez <aurelio@seritium.es>
2023-01-14 12:14:48 +0100

    Funcionalidad y movimiento conseguidos

--- main.ts
+++ main.ts
@@ -1,41 +1,51 @@
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile0`, function (sprite, location) {
+    music.wawawawaa.play()
+    game.showLongText("Terminaste. Has conseguido " + info.score() + "Puntos", DialogLayout.Full)
+    game.over(true)
+})
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
-    Bola.y += -1
-})
-controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
-    Bola.x += 1
-})
-controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
-    Bola.x += -1
-})
-controller.down.onEvent(ControllerButtonEvent.Repeated, function () {
     Bola.vy += 1
     if (Bola.vy > maxV) {
         Bola.vy = maxV
     }
 })
-controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
-    Bola.y += 1
-})
-controller.right.onEvent(ControllerButtonEvent.Repeated, function () {
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     Bola.vx += 1
     if (Bola.vx > maxV) {
         Bola.vx = maxV
     }
 })
-controller.up.onEvent(ControllerButtonEvent.Repeated, function () {
-    Bola.vy += -1
-    if (Bola.vy < -1 * maxV) {
-        Bola.vy = -1 * maxV
-    }
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile`, function (sprite, location) {
+    music.knock.play()
+    info.changeScoreBy(-1)
 })
-controller.left.onEvent(ControllerButtonEvent.Repeated, function () {
+controller.down.onEvent(ControllerButtonEvent.Repeated, function () {
+    Bola.vx = 0
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     Bola.vx += -1
     if (Bola.vx < -1 * maxV) {
         Bola.vx = -1 * maxV
     }
 })
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    Bola.vy += -1
+    if (Bola.vy < -1 * maxV) {
+        Bola.vy = -1 * maxV
+    }
+})
+controller.right.onEvent(ControllerButtonEvent.Repeated, function () {
+    Bola.vy = 0
+})
+controller.up.onEvent(ControllerButtonEvent.Repeated, function () {
+    Bola.vx = 0
+})
+controller.left.onEvent(ControllerButtonEvent.Repeated, function () {
+    Bola.vy = 0
+})
 let maxV = 0
 let Bola: Sprite = null
+info.setScore(2000)
 scene.setBackgroundColor(9)
 Bola = sprites.create(img`
     . . 6 6 6 6 . . 
@@ -45,6 +55,14 @@ Bola = sprites.create(img`
     . c b b b d c . 
     . . c c c c . . 
     `, SpriteKind.Player)
+Bola.setPosition(70, 120)
 maxV = 10
 tiles.setCurrentTilemap(tilemap`nivel1`)
+tiles.placeOnTile(Bola, tiles.getTileLocation(8, 15))
 scene.cameraFollowSprite(Bola)
+forever(function () {
+    if (info.score() <= 0) {
+        game.showLongText("Lo siento. Te has quedado sin puntos", DialogLayout.Full)
+        game.over(false)
+    }
+})
